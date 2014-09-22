72a5a669e5e8957775ba74ec218c578f40a12fb0
Vishwanath Arondekar <vishwanath.arondekar@xoriant.com>
2014-09-22 12:28:32 +0530

    Updated message

--- tasks/spriteGenerator.js
+++ tasks/spriteGenerator.js
@@ -154,12 +154,12 @@ module.exports = function(grunt) {
             var message = null;
             if(options.overwrite){
             	filepath = filepath.replace('.css', '-sprited.css');
-            	message = 'File updated with image references has been created at path ', 'generated/' +filepath;
+            	message = 'File updated with image references has been created at path ', filepath;
             } else {
             	message = 'File', filepath, 'has been updated';
             }
             grunt.file.write(filepath, data);
-            grunt.log.writeln('File updated with image references has been created at path ', 'generated/' +filepath);
+            grunt.log.writeln(message);
         };
 
 
